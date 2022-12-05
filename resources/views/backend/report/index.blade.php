0f0e02f5f06cfb1c506703b709dded43d94d870e
ex-dec <bighadi123@yahoo.com>
2022-12-05 09:38:07 +0700

    fix sidebar and report page

--- resources/views/backend/report/index.blade.php
+++ resources/views/backend/report/index.blade.php
@@ -26,7 +26,7 @@
                                             Contact</th>
                                         <th
                                             class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                            is_done</th>
+                                            Status</th>
                                     </tr>
                                 </thead>
                                 <tbody>
