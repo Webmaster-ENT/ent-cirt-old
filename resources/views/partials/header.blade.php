25927b801302b1c28ede6b9ddedee92b2c3677d9
ktyudha <ktyudha@gmail.com>
2022-12-06 08:54:02 +0700

    style ckeditor

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,5 +8,4 @@
 <script src="https://unpkg.com/@popperjs/core@2"></script>
 <!-- Main Styling -->
 <link href="{{ asset('tailwind/assets/css/soft-ui-dashboard-tailwind.css') }}" rel="stylesheet" />
-<!-- CKEDITOR -->
-<script src="https://cdn.ckeditor.com/ckeditor5/35.3.2/classic/ckeditor.js"></script>
+<link href="{{ asset('style.css') }}" rel="stylesheet" />
