532044d512284ba3e2c5286f2b10bc43585d00d2
ramadan-202 <syahrul.oke21@gmail.com>
2022-12-23 16:41:50 +0700

    fix navbar landingpage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Cyber Incident Response Team</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    
+
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+        <link rel="stylesheet" href="{{ asset('css/mdb.min.css') }}" />
+
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!-- ======= Header ====== -->
+    <header>
+        <!-- Intro settings -->
+        <style>
+            #intro {
+                /* Margin to fix overlapping fixed navbar */
+                margin-top: 58px;
+            }
+
+            @media (max-width: 991px) {
+                #intro {
+                    /* Margin to fix overlapping fixed navbar */
+                    margin-top: 45px;
+                }
+            }
+        </style>
+
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #1751a5;">
+            <div class="container">
+                <!-- Navbar brand -->
+                <a class="navbar-brand" href=" {{ route('index') }}">
+                    <img src=https://www.pens.ac.id/wp-content/uploads/2018/04/Logo-PENS-putih-e1522932523588.png
+                        height="35" alt="" loading="lazy" style="margin-top: -3px; " />
+                </a>
+                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+                    data-mdb-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarExample01">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link text-white" aria-current="page" href="/">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/#news" rel="nofollow">Artikel</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/#about">Tentang Kami</a>
+                        </li>
+                    </ul>
+
+
+                </div>
+            </div>
+        </nav>
+        <!-- Navbar -->
+
+        <!-- Jumbotron -->
+
+        <!-- Jumbotron -->
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+            <div class="row justify-content-center">
+                <div class="col-xl-7 col-lg-9 text-center">
+                    <h1>Cyber Incident Response Team</h1>
+                    <h2>Politeknik Elektronika Negeri Surabaya</h2>
+                </div>
+            </div>
+            <div class="text-center">
+                <a href="#news" class="btn-get-started scrollto">Mulai</a>
+            </div>
+
+            <div class="row icon-boxes">
+                <div class="col-md-6 col-lg-6 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
+                    data-aos-delay="200">
+                    <div class="icon-box">
+                        <div class="icon"><i class="bi bi-journal-text"></i></div>
+                        <h4 class="title">
+                            <a data-toggle="modal" data-target="#exampleModal">Pengaduan</a>
+                        </h4>
+                        <p class="description">Pengaduan Keamanan Siber</p>
+                    </div>
+                </div>
+
+{{--                <div class="col-md-6 col-lg-6 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"--}}
+{{--                    data-aos-delay="300">--}}
+{{--                    <div class="icon-box">--}}
+{{--                        <div class="icon"><i class="ri-stack-line"></i></div>--}}
+{{--                        <h4 class="title"><a href="#news">News</a></h4>--}}
+{{--                        <p class="description">Berita Laporan </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+                <div class="col-md-6 col-lg-6 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
+                    data-aos-delay="400">
+                    <div class="icon-box">
+                        <div class="icon"><i class="ri-article-line"></i></div>
+                        <h4 class="title"><a href="#news">Artikel</a></h4>
+                        <p class="description">Artikel Mengenai Keamanan Siber</p>
+                    </div>
+                </div>
+
+                <!-- <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="500">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-fingerprint-line"></i></div>
+            <h4 class="title"><a href="">Nemo Enim</a></h4>
+            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+          </div>
+        </div> -->
+
+            </div>
+        </div>
+    </section><!-- End Hero -->
+
+    <main id="main">
+        <!-- ======= news Section ======= -->
+        <section id="news" class="news">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                    <h2>Artikel</h2>
+                    <!-- <p>Mag  nam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
+                </div>
+
+                {{--  <div class="row" data-aos="fade-up" data-aos-delay="150">
+                    <div class="col-lg-12 d-flex justify-content-center">
+                        <ul id="news-flters">
+                            <li data-filter="*" class="filter-active">All</li>
+                            <li data-filter=".filter-app">Hoax</li>
+                            <li data-filter=".filter-card">Article</li>
+                            <!-- <li data-filter=".filter-web">Announcement</li> -->
+                        </ul>
+                    </div>
+                </div>  --}}
+
+                <div class="row news-container " data-aos="fade-up" data-aos-delay="300">
+                    @foreach ($articles as $article)
+                        <div class="col-lg-4 col-md-6 news-item filter-card">
+                            <div class="news-wrap">
+                                <img src="{{ asset('storage/images/' . $article->thumbnail_url) }}" class="img-fluid"
+                                    alt="">
+                                <div class="news-info">
+                                    <h4>{{ $article->title }}</h4>
+                                    <p> {{ $article->updated_at->format('F j, Y') }}</p>
+                                    <div class="news-links">
+                                        <a href="{{ asset('storage/images/' . $article->thumbnail_url) }}"
+                                            data-gallery="newsGallery" class="d"><i class="bx bx-plus"></i></a>
+                                        <a href="{{ route('artikel.show', $article) }}" title="More Details"><i
+                                                class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+
+                </div>
+
+            </div>
+        </section>
+        <!-- End news Section -->
+
+        <!-- ======= About Video Section ======= -->
+        <section id="about" class="about">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                    <h3>Siapakah kita?</h3>
+                    <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. </p>  -->
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-6 video-box align-self-baseline" data-aos="fade-right" data-aos-delay="100">
+                        <img style="max-width:80%" src="assets/img/Logo-ENT-Gelap.svg" alt="">
+                        <!-- <a href="https://www.youtube.com/watch?v=IVJ4Hsv1MIM&ab_channel=KantahKabBanjarnegara" class="glightbox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>  -->
+                    </div>
+
+                    <div class="col-lg-6 pt-3 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
+                        <h3>Cyber Incident Response Team.</h3>
+                        <!-- <p class="fst-italic" style="font-size:16px;">
+              Cyber Incident Response Team Politeknik Elektronika Negeri Surabaya (CIRT PENS).
+            </p > -->
+                        <p style="font-size:18px;">
+                            Misi dibentuknya C.I.R.T ( Cyber Insident Response Team ) adalah untuk :
+
+                        <p>1. Mengkordinasikan, mengoperasionalkan sistem mitigasi, manajemen krisis, penanggulangan dan
+                            pemulihan terhadap insiden keamanan siber pada lingkungan Politeknik Elektronika Negeri
+                            Surabaya</p>
+
+                        <p>2. Membangun kerja sama dalam rangka penanggulangan dan pemulihan insiden keamanan siber di
+                            lingkungan Politeknik Elektronika Negeri Surabaya</p>
+                        <p>3. Membangun kapasitas sumber daya penanggulangan dan pemulihan insiden keamanan siber di
+                            lingkungan Politeknik Elektronika Negeri Surabaya.</p>
+
+
+                        </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </section>
+        <!-- End About Video Section -->
+
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Form Pengaduan</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('report.store') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            @method('post')
+                            <div class="form-group">
+                                <label for="subject" class="col-form-label">Topik :</label>
+                                <input type="text" class="form-control" name="subject" id="subject">
+                            </div>
+                            <div class="form-group">
+                                <label for="body" class="col-form-label">Deskripsi :</label>
+                                <textarea class="form-control" id="body" name="body"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="contact" class="col-form-label">Kontak (Opsional) :</label>
+                                <input type="text" class="form-control" id="contact" name="contact">
+                            </div>
+                            <div class="form-group">
+                                <label for="image_url" class="col-form-label">Gambar Pendukung (Opsional) :</label>
+                                <input type="file" class="form-control" id="image_url" name="image_url">
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="col-form-label"></label>
+                                <input type="hidden" class="form-control" id="1">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                                <button type="submit" class="btn btn-primary">Kirim Pesan</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main><!-- End #main -->
+
+    <!-- End Footer -->
+    <div class="text-center p-3" style="background-color:  #1751a5;">
+        <a class="text-white" href="">© 2022 Politeknik Elektronika Negeri Surabaya</a>
+    </div>
+    <div id="preloader"></div>
+    <!-- <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> -->
+    <a type="button" data-toggle="modal" data-target="#exampleModal"
+        class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-journal-text"></i>
+
+    </a>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script type="text/javascript" src="{{ asset('js/mdb.min.js') }}"></script>
+
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
